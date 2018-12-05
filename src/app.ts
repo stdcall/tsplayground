15d07f3c3fedcc9cb14a12cd5aeccc5b07557531
Nikolay Hodyunya <nkhodyunya@gmail.com>
2018-12-05 19:39:03 +0700

    123

--- src/app.ts
+++ src/app.ts
@@ -4,13 +4,15 @@ import "./styles.scss";
 const observable1 = Observable.create(observer => {
     observer.next(5);
     observer.next(4);
-    setTimeout(x => {observer.next(3)}, 1000);
+    setTimeout(x => {observer.next(3); observer.next(20); observer.compelete()}, 1000);
+    
 });
 
 const observable2 = Observable.create(obs => {
     obs.next('five');
     obs.next('four');
     obs.next('three');
+    setTimeout(x => {obs.next('two'), obs.complete()}, 2000);
 });
  
-combineLatest(observable1, observable2).subscribe((x) => { console.log(x) });
+combineLatest(observable1, observable2).subscribe((x) => { console.log(`${x[0]}:${x[1]}`) });
