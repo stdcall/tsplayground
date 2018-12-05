e22944e07a5c702081dc9c644ada2aebce07d3f3
Nikolay Hodyunya <nkhodyunya@gmail.com>
2018-12-05 12:38:25 +0700

    123.

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,16 @@
+import { Observable, of, combineLatest, zip } from 'rxjs';
 import "./styles.scss";
 
-console.log('called from app.ts!!!');
+const observable1 = Observable.create(observer => {
+    observer.next(5);
+    observer.next(4);
+    setTimeout(x => {observer.next(3)}, 1000);
+});
+
+const observable2 = Observable.create(obs => {
+    obs.next('five');
+    obs.next('four');
+    obs.next('three');
+});
+
+combineLatest(observable1, observable2).subscribe((x) => { console.log(x) });
